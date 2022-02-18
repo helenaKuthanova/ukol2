b2ebde817da9d1946bb827e6dcec3127be5b0cdd
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-18 15:36:25 +0100

    funkce zvětší se písmo

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function odstavecZcervena() {
     let odstavec = document.querySelector("p")
     odstavec.classList.toggle("cerveny")
 }
+
+let velikost = 16
+function zvetsiPismo() {
+    let odstavec = document.querySelector("p");
+    velikost++
+    odstavec.style.fontSize=velikost + "px"
+}
