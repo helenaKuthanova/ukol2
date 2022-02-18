7414007a959fbe19671b36a3d0a13488348d4242
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-18 16:52:47 +0100

    funkce ovládání zvuku

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 function priNajetiMysiZtucni() {
-    let odstavec = document.querySelector("p")
-    odstavec.classList.toggle("tucny")
+    let odstavec = document.querySelector("p");
+    odstavec.classList.toggle("tucny");
 }
 
 function odstavecZcervena() {
-    let odstavec = document.querySelector("p")
-    odstavec.classList.toggle("cerveny")
+    let odstavec = document.querySelector("p");
+    odstavec.classList.toggle("cerveny");
 }
 
 let velikost = 16
 function zvetsiPismo() {
     let odstavec = document.querySelector("p");
-    velikost++
-    odstavec.style.fontSize=velikost + "px"
+    velikost++;
+    odstavec.style.fontSize=velikost + "px";
+}
+
+function prehrajAudio() {
+    let zvuk = document.getElementById("zvukovaStopa");
+    zvuk.play();
+}
+
+function zastavAudio() {
+    let zvuk = document.getElementById("zvukovaStopa");
+    zvuk.pause();
+}
+
+function hrajOdZacatku() {
+    let zvuk = document.getElementById("zvukovaStopa")
+    zvuk.currentTime = 0;
+    zvuk.play()
+}
+
+function nastavHlasitost(hlasitost) {
+    let zvuk = document.getElementById("zvukovaStopa")
+    zvuk.volume = hlasitost
 }
