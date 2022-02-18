c132d396d127a48dd6797e3b479affc6a665cd78
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-18 15:09:26 +0100

    funkce odstavec zčervená

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function priNajetiMysiZtucni() {
     let odstavec = document.querySelector("p")
     odstavec.classList.toggle("tucny")
 }
+
+function odstavecZcervena() {
+    let odstavec = document.querySelector("p")
+    odstavec.classList.toggle("cerveny")
+}
