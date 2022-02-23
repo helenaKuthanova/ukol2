d77c7f279f629434f5ff0489922dc0d652d6920b
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-23 11:24:24 +0100

    upravy podle Matejova reseni na lekci

--- app.js
+++ app.js
@@ -3,35 +3,44 @@ function priNajetiMysiZtucni() {
     odstavec.classList.toggle("tucny");
 }
 
-function odstavecZcervena() {
-    let odstavec = document.querySelector("#palindrom");
+/* v html mám v parametru "#palindrom" - jako selektor používám ID */
+function odstavecZcervena(selektorOdstavce) {
+    let odstavec = document.querySelector(selektorOdstavce);
     odstavec.classList.toggle("cerveny");
 }
 
+/* v html mám v parametru "palindrom" - z použití fukce get..ById je jasné, že předávám ID*/
+
+/** Funkce zvětší/zmenší písmo po kliknutí;
+* zavináčem definuji parametry (protože JS je netypovaný jazyk, určuji, jaký má být typ parametrů)
+* @param {string} idOdstavce
+* @param {int} zmenaVelikosti
+*/
+
 let velikost = 16
-function zvetsPismo() {
-    let odstavec = document.querySelector("#palindrom");
-    velikost++;
+function zvetsPismo(idOdstavce, zmenaVelikosti) {
+    let odstavec = document.getElementById(idOdstavce);
+    velikost += zmenaVelikosti;
     odstavec.style.fontSize=velikost + "px";
 }
 
-function prehrajAudio() {
-    let zvuk = document.getElementById("zvukovaStopa");
+function prehrajAudio(idZvuku) {
+    let zvuk = document.getElementById(idZvuku);
     zvuk.play();
 }
 
-function zastavAudio() {
-    let zvuk = document.getElementById("zvukovaStopa");
+function zastavAudio(idZvuku) {
+    let zvuk = document.getElementById(idZvuku);
     zvuk.pause();
 }
 
-function hrajOdZacatku() {
-    let zvuk = document.getElementById("zvukovaStopa");
+function hrajOdZacatku(idZvuku) {
+    let zvuk = document.getElementById(idZvuku);
     zvuk.currentTime = 0;
     zvuk.play();
 }
 
-function nastavHlasitost(hlasitost) {
-    let zvuk = document.getElementById("zvukovaStopa");
+function nastavHlasitost(idZvuku, hlasitost) {
+    let zvuk = document.getElementById(idZvuku);
     zvuk.volume = hlasitost;
 }
